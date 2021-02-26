173f69115c5232f7d05db074027d87691f33f876
Paul Silavong <conjonction-mac@MacBook-Air-de-Paul.local>
2021-02-26 19:08:54 +0100

    add detail noscommerces + screenshots

--- src/pages/Noscommerces/index.js
+++ src/pages/Noscommerces/index.js
@@ -1,5 +1,6 @@
 import { lazy } from "react";
 import { Row, Col } from "antd";
+import { Image } from 'antd';
 
 const Container = lazy(() => import("../../common/Container"));
 
@@ -9,18 +10,53 @@ const Noscommerces = () => {
             <Container>
                 <h4>Nos Commerces</h4>
                 <Row type="flex" justify="space-between" align="middle">
-                <Col lg={11} md={11} sm={12} xs={24}>
-                    slider
-                </Col>
-                <Col lg={11} md={11} sm={12} xs={24}>
-                    description
-                    <Col lg={6}>
-
+                    <Col lg={24}>
+                        <h4>Une façon simple de vendre en ligne</h4>
+                            <p>Une plateforme locale "e-commerce" offrant aux commerçants, aux créateurs, aux artisans, aux prestataires de services et aux collectivités une présence en ligne collective. Elle permettra en autre des animations mutualisées sont déployées auprès des habitants pour faire la promotion de ces commerces.</p>
+                        <h4>Fonctionnalités :</h4>
                     </Col>
-                    <Col lg={6}>
-                        
+                </Row>
+                <Row type="flex" justify="space-between" align="middle">
+                    <Col lg={12}>
+                        <h5>Pour les commerçants :</h5>
+                        <ul>
+                            <li>Une page web dédiée personalisable</li>
+                            <li>Notification lors d'une commande par email et sur le site</li>
+                            <li>Pouvoir administrer les informations de livraison et de Click&Collect</li>
+                            <li>Fonctionnalité d'aide à la gestion et à l'analyse du chiffre d'affaire</li>
+                        </ul>
                     </Col>
-                </Col>
+                    <Col lg={12}>
+                        <h5>Pour les usagers :</h5>
+                        <ul>
+                            <li>Une recherche des produits par quartiers</li>
+                            <li>Paiement en ligne sécurisé avec Stripe</li>
+                            <li>Navigatition intuitive</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row type="flex" justify="space-between" align="middle">
+                    <Col lg={24} md={11} sm={12} xs={24}>
+                        <Row type="flex" justify="space-between" align="middle">
+                        <Col lg={24}>
+                        <h4>Aperçu :</h4>
+                        <Image.PreviewGroup>
+                            <Image
+                                width={200}
+                                src="/img/png/noscommerces1.png"
+                            />
+                            <Image
+                                width={200}
+                                src="/img/png/noscommerces2.png"
+                            />
+                            <Image
+                                width={200}
+                                src="/img/png/noscommerces3.png"
+                            />
+                           </Image.PreviewGroup>
+                           </Col>
+                        </Row>
+                    </Col>
                 </Row>
             </Container>
         </>
